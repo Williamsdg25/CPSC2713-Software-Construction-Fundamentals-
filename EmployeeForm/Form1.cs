6e330835661909619f8b605ebcd601725423a839
Ken Scanlon <40871029+scanlon771@users.noreply.github.com>
2019-09-02 22:47:10 -0700

    Homework - M6

---
+++ EmployeeForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EmployeeForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProductionWorker obj = new ProductionWorker();
+
+            obj.name = textBox1.Text;
+            obj.number = textBox2.Text;
+            obj.shiftNumber = Convert.ToInt16(textBox3.Text);
+            obj.payRate = Convert.ToDouble(textBox4.Text);
+
+            MessageBox.Show("Employee Name: " + obj.name + "\n" + "Employee Number: " + obj.number + "\n" 
+                + "Employee Shift: " + obj.shiftNumber + "\n" + "Employee Pay Rate: $" + obj.payRate);
+        }
+    }
+
+    public class Employee
+        {
+            public string name;
+            public string number;
+        }
+
+        public class ProductionWorker : Employee 
+        {
+            public int shiftNumber;
+            public double payRate;
+        }
+
+    
+}
